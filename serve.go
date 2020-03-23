41806183343d2d0506261286ee7e53a7cbf92e2d
David Šorm <david.sorm@gmail.com>
2020-03-23 20:16:29 +0100

    separation of article into it's own file

--- serve.go
+++ serve.go
@@ -14,18 +14,6 @@ type BlogInfo struct {
 	Name string
 }
 
-type Article struct {
-	Name string
-	// type template.HTML allows unescaped html
-	Content template.HTML
-}
-
-type ArticleView struct {
-	Info    BlogInfo
-	Article *Article
-	RootURL string
-}
-
 var articles []*Article
 
 var templates []*template.Template
@@ -36,40 +24,6 @@ const (
 	templateIndex = iota
 	templateArticle
 )
-
-func handleArticle(rw http.ResponseWriter, req *http.Request) {
-	// split the uri (example: /articles/1 )
-	split := strings.Split(req.RequestURI, "/")
-
-	// make sure there are 3 splits
-	if len(split) != 3 {
-		handle404(rw, req)
-		return
-	}
-
-	// convert to number
-	i, err := strconv.Atoi(split[2])
-	if err != nil {
-		handle404(rw, req)
-		return
-	}
-
-	// make sure article with the number exists
-	if articles[i] == nil {
-		handle404(rw, req)
-		return
-	}
-
-	// respond
-	articleView := ArticleView{
-		Info:    blogInfo,
-		Article: articles[i],
-		RootURL: "//" + req.Host + "/",
-	}
-	if err := templates[templateArticle].Execute(rw, articleView); err != nil {
-		fmt.Println("Error while parsing template:", err.Error())
-	}
-}
 
 func handleCss(rw http.ResponseWriter, req *http.Request) {
 
