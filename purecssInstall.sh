5646b60ffbbc3ac532ae9e6c84350b4fbb832bf6
David Sorm <david.sorm@gmail.com>
2020-08-07 23:00:07 +0200

    fixed purecssInstall.sh

--- purecssInstall.sh
+++ purecssInstall.sh
@@ -12,4 +12,4 @@ cp tmp/pure-release-1.0.1/*min.css html/css/pure/.
 # don't forget to include copy of PureCSS' license
 cp tmp/pure-release-1.0.1/LICENSE.md html/css/pure/LICENSE.md
 
-#rm -r tmp
+rm -r tmp
